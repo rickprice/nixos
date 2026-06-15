033d19b31cfd10bef253a4e7672060614db41919
Frederick Price <fprice@pricemail.ca>
2026-06-15 02:57:12 -0400

    Remove that change again

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -106,8 +106,6 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
