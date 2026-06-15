be2abc03217d28864440b4575e18fc44726c1871
Frederick Price <fprice@pricemail.ca>
2026-06-15 17:30:56 -0400

    Grab Debian patches for midisnoop

--- config/packages/midisnoop.nix
+++ config/packages/midisnoop.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, qmake, wrapQtAppsHook, qtbase, qttools, rtmidi }:
+{ lib, stdenv, fetchFromGitHub, qmake, wrapQtAppsHook, qtbase, qttools, rtmidi, pkg-config }:
 
 stdenv.mkDerivation {
   pname = "midisnoop";
@@ -11,20 +11,22 @@ stdenv.mkDerivation {
     hash = "sha256-XDpDfTq5sAHqeYk3j+HA9H0SGq5sawrLzdK2rqN9fv4=";
   };
 
-  nativeBuildInputs = [ qmake wrapQtAppsHook ];
+  patches = [
+    # typedef RtMidiError RtError (renamed in rtmidi 2.1) + drop WINDOWS_KS
+    ./patches/03-fix_build_with_rtmidi_2_1.patch
+    # use PKGCONFIG += rtmidi so qmake picks up include paths via pkg-config
+    ./patches/04-rtmidi-pkgconfig.patch
+    # add explicit #include <QtCore/QObject> to fix Qt5 MOC staticMetaObject error
+    ./patches/0005-src-engine.h-another-qt5-fix.patch
+  ];
+
+  nativeBuildInputs = [ qmake wrapQtAppsHook pkg-config ];
   buildInputs = [ qtbase qttools rtmidi ];
 
   postPatch = ''
-    # Desktop file install runs a Python script that bakes in absolute paths at
-    # build time; skip it and only install the binary.
+    # Desktop file install runs a Python script that bakes in absolute paths;
+    # skip it and only install the binary.
     sed -i '/INSTALLS += desktop/d' src/src.pro
-    # qmake doesn't pick up external headers via NIX_CFLAGS_COMPILE, so patch
-    # the rtmidi include and library paths directly into the project file.
-    sed -i 's|LIBS += -lrtmidi|LIBS += -L${lib.getLib rtmidi}/lib -lrtmidi\nINCLUDEPATH += ${lib.getDev rtmidi}/include/rtmidi|' src/src.pro
-    # RtError was renamed to RtMidiError in rtmidi 4.x
-    sed -i 's/RtError/RtMidiError/g' src/engine.cpp
-    # WINDOWS_KS was removed from RtMidi::Api in rtmidi 6.x
-    sed -i '/case RtMidi::WINDOWS_KS:/,+2d' src/engine.cpp
   '';
 
   qmakeFlags = [ "PREFIX=${placeholder "out"}" ];
