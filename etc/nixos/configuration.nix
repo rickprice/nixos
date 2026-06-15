4954f46ee84a595a0850c9d2733d84734d68e12a
Frederick Price <fprice@pricemail.ca>
2026-06-14 23:13:37 -0400

    Shouldn't need to do this

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -139,7 +139,7 @@
   };
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 22 ];
+  # networking.firewall.allowedTCPPorts = [ 22 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
